cb08d535dc3c0b84a5819a4ee0187a046eceb93e
Philipp Brumm <philipp@funkensturm.de>
2016-09-06 10:44:10 +0200

    better quality sorting

--- src/utils/byQuality.js
+++ src/utils/byQuality.js
@@ -1,6 +1,14 @@
 
-const byQuality = ({ quality: qualityA }, { quality: qualityB }) => (
-  +qualityA.slice(0, -1) < +qualityB.slice(0, -1) ? 1 : -1
-)
+const sortOrder = [
+  '1080p',
+  '720p',
+  '360p',
+  'Auto',
+  'auto',
+]
+
+const byQuality = ({ quality: qualityA }, { quality: qualityB }) => {
+  return sortOrder.indexOf(qualityA) < sortOrder.indexOf(qualityB) ? -1 : 1
+}
 
 export default byQuality
