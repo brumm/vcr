697bd156fe2449835918593ea3c7289e21c9ef78
Philipp Brumm <philipp@funkensturm.de>
2016-09-14 11:47:53 +0200

    now that we're passing location in async-props, pass this onto the promise handler for promised

--- src/utils/promised.js
+++ src/utils/promised.js
@@ -1,7 +1,7 @@
 export default function promised(target, key, descriptor) {
   let method = descriptor.initializer()
-  descriptor.initializer = () => ({ params }, callback) => {
-    method(params)
+  descriptor.initializer = () => ({ params, location }, callback) => {
+    method(params, location)
       .then((...args) => callback(null, ...args))
       .catch(error => {
         if (process.env.DEV) {
