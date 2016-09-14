0e8c43cf33f29ae5183b082e09e239bf34fa3463
Philipp Brumm <philipp@funkensturm.de>
2016-09-14 11:46:21 +0200

    use legit-image to fade images in after loading

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Image from 'legit-image'
 
 import style from './Poster.scss'
 
 const Poster = ({url, active, shrink}) => (
   <div className={active ? style.ContainerActive : style.Container}>
-    <img src={url} referrerPolicy='no-referrer' className={style.Poster} />
+    <Image src={url} referrerPolicy='no-referrer' className={style.Poster} />
   </div>
 )
 
