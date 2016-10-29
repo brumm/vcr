c9d47ec19f72a3e903c04c1f1b67db6735da7542
Philipp Brumm <philipp@railslove.com>
2016-10-29 20:31:20 +0200

    d'oh

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -3,8 +3,9 @@ import Image from 'legit-image'
 
 import style from './Poster.scss'
 
-const Poster = ({url, active, shrink}) => (
+const Poster = ({url, active, shrink, children}) => (
   <div className={active ? style.ContainerActive : style.Container}>
+    {children}
     <Image src={url} referrerPolicy='no-referrer' className={style.Poster} />
   </div>
 )
