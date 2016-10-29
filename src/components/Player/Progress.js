469b813b0a4d8b77a2def24dd3168f02964ebc6d
Philipp Brumm <philipp@railslove.com>
2016-10-29 13:20:46 +0200

    visualize loaded percentage on seek bar

--- src/components/Player/Progress.js
+++ src/components/Player/Progress.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { withMediaProps } from 'react-media-player'
+import Flex from 'flex-component'
+
 import style from './Player.scss'
 
 @withMediaProps
@@ -8,8 +10,11 @@ export default class Progress extends React.Component {
     return this.props.media.currentTime !== media.currentTime
   }
 
-  handleSeek({ clientX, target: { offsetLeft, clientWidth }}) {
-    let fraction = (clientX - offsetLeft) / clientWidth
+  handleSeek({ clientX, target }) {
+    const { clientWidth } = target
+    const { left } = target.getBoundingClientRect()
+    let fraction = (clientX - left) / clientWidth
+
     this.props.media.seekTo(
       this.props.media.duration * fraction
     )
@@ -18,12 +23,22 @@ export default class Progress extends React.Component {
   render() {
     const { media } = this.props
     return (
-      <progress
-        onClick={::this.handleSeek}
-        className={style.Progress}
-        max={100}
-        value={media.currentTime * 100 / media.duration}
-      />
+      <Flex grow={1} onClick={::this.handleSeek} className={style.ProgressContainer} alignItems='center'>
+        <Flex grow={1} className={style.Progress}>
+          <progress
+            className={style.ProgressPlayed}
+            max={100}
+            value={media.currentTime * 100 / media.duration}
+          />
+
+          <progress
+            className={style.ProgressLoaded}
+            style={style}
+            max={100}
+            value={media.progress * 100}
+          />
+        </Flex>
+    </Flex>
     )
   }
 }
